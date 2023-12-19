3d4506c39359b179a49ba2c5ecbcb492810735e4
MakeTrade <44473011+MakeTrade@users.noreply.github.com>
2023-12-19 03:43:50 +0000

    Update total-supply.ts

--- api/total-supply.ts
+++ api/total-supply.ts
@@ -9,12 +9,19 @@ export default async (request: VercelRequest, response: VercelResponse) => {
   const provider = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/eth')
 
   const nationContract = new ethers.Contract(
-    process.env.NATION_ADDRESS.sub.VENATION_ADDRESS as string,
+    process.env.NATION_ADDRESS as string,
     ERC20.abi,
     provider
   )
 
-  const totalSupply: BigNumber = await nationContract.totalSupply()
+  const supplies: BigNumber[] = await Promise.all([
+    nationContract.maxSupply(),
+    nationContract.balanceOf(process.env.VENATION_ADDRESS),
+  ])
+
+  const [maxSupply, veNationSupply] = supplies
+
+  const totalSupply = maxSupply.sub(veNationSupply)
 
   response.status(200).send(ethers.utils.formatUnits(totalSupply, 18))
 }
